52705a59a8d58f76fad78c7e865d56bb10792e8a
pelle04 <107267826+pelle04@users.noreply.github.com>
2022-12-14 15:42:50 -0700

    primo_commit_progetto_giusto

---
+++ pellegrinoAgnati/pellegrinoAgnati/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace pellegrinoAgnati
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly ImageSource[] quadratini = new ImageSource[]
+        {
+            new BitmapImage(new Uri("Assets/TileRed.png",UriKind.Relative))
+        };
+
+        private readonly ImageSource[] blocchi = new ImageSource[]
+        {
+            new BitmapImage(new Uri("Assets/Block-Z.png",UriKind.Relative))
+        };
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void GameCanvas_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void window_keyDown(object sender, KeyEventArgs e)
+        {
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+ 
+    }
+}
---
+++ pellegrinoAgnati/pellegrinoAgnati/grigliaDiGioco.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pellegrinoAgnati
+{
+    internal class grigliaDiGioco
+    {
+        public int righe { get; set; }
+        public int colonne { get; set; }
+
+        private int myVar;
+
+        public int MyProperty
+        {
+            get { return myVar; }
+            set { myVar = value; }
+        }
+
+    }
+}
