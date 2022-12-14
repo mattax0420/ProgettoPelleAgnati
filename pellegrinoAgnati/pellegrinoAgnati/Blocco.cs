3587df97faa312c169965f528f858f958335fd7b
mattax0420 <81468690+mattax0420@users.noreply.github.com>
2022-12-14 17:53:31 +0100

    aggiunta e scrittura della Classe pos e blocco

--- pellegrinoAgnati/pellegrinoAgnati/Blocco.cs
+++ pellegrinoAgnati/pellegrinoAgnati/Blocco.cs
@@ -6,7 +6,50 @@ using System.Threading.Tasks;
 
 namespace pellegrinoAgnati
 {
-    internal class Blocco
+    public abstract class Blocco
     {
+        protected abstract Pos[][] Blocchetto { get; } //piastra che costituisce un intera forma
+        protected abstract Pos InizioOffset { get; }
+        public abstract int IDBlocco { get; } //ID della forma costituita dai blocchetti
+
+        private int StatoRotazione; //rotazione del blocco
+        private Pos offset;
+        public Blocco()
+        {
+            offset = new Pos(InizioOffset.Riga, InizioOffset.Colonna);
+        }
+
+        public IEnumerable<Pos> BlocchettoPos()
+        {
+            foreach (Pos p in Blocchetto[StatoRotazione])
+            {
+                yield return new Pos(p.Riga + offset.Riga, p.Colonna + offset.Colonna); //yield specifica il valore successivo
+            }
+        }
+
+        public void RotazioneSinistra()
+        {
+            StatoRotazione = (StatoRotazione + 1) % Blocchetto.Length;
+        }
+        public void RotazioneDestra()
+        {
+            if(StatoRotazione == 0)
+                StatoRotazione=Blocchetto.Length-1;
+            else
+                StatoRotazione--;
+        }
+        public void Movimento(int rig,int colon)
+        {
+            offset.Riga += rig;
+            offset.Colonna += colon;
+        }
+
+        public void ResetBlocco()
+        {
+            StatoRotazione = 0;
+            offset.Riga = InizioOffset.Riga;
+            offset.Colonna=InizioOffset.Colonna;
+        }
+
     }
 }
---
+++ pellegrinoAgnati/pellegrinoAgnati/Pos.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pellegrinoAgnati
+{
+    public class Pos
+    {
+        public int Riga { get; set; }
+        public int Colonna { get; set; }
+
+        public Pos(int rig, int colon)
+        {
+            Riga = rig;
+            Colonna = colon;
+        }   
+    }
+}
