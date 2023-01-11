646e3f00dbcbae92bcb627b00b1c07856299906f
mattax0420 <81468690+mattax0420@users.noreply.github.com>
2023-01-11 10:35:03 +0100

    implementazione degli ultimi blocchi

---
+++ pellegrinoAgnati/pellegrinoAgnati/BloccoL.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pellegrinoAgnati
+{
+    public class BloccoL : Blocco
+    {
+        private Pos[][] blocchetti = new Pos[][]
+        {
+             new Pos[] {new(0,2), new(1,0), new (1,1), new(1,2)},  //primo stato di rotazione
+             new Pos[] {new(0,1), new(1,1), new (2,1), new(2,2)},  //secondo stato di rotazione
+             new Pos[] {new(1,0), new(1,1), new (1,2), new(2,0)},  //terzo stato di rotazione
+             new Pos[] {new(0,0), new(0,1), new (1,1), new(2,1)},  //quarto stato di rotazione
+        };
+
+
+        public override int IDBlocco => 3; //identificatore del blocco per utilizzo del random
+
+        protected override Pos[][] Blocchetto => blocchetti; //in base alla rotazione grazie al get nella classe Blocco varierà la posizione dei blocchetti
+
+        protected override Pos InizioOffset => new Pos(0,3); //posizione iniziale 
+    }
+}
---
+++ pellegrinoAgnati/pellegrinoAgnati/BloccoT.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pellegrinoAgnati
+{
+    public class BloccoT : Blocco
+    {
+        private Pos[][] blocchetti = new Pos[][]
+        {
+             new Pos[] {new(0,1), new(1,0), new (1,1), new(1,2)},  //primo stato di rotazione
+             new Pos[] {new(0,1), new(1,1), new (1,2), new(2,1)},  //secondo stato di rotazione
+             new Pos[] {new(1,0), new(1,1), new (1,2), new(2,1)},  //terzo stato di rotazione
+             new Pos[] {new(0,1), new(1,0), new (1,1), new(2,1)},  //quarto stato di rotazione
+        };
+
+        public override int IDBlocco => 6; //identificatore del blocco per utilizzo del random
+
+        protected override Pos[][] Blocchetto => blocchetti; //in base alla rotazione grazie al get nella classe Blocco varierà la posizione dei blocchetti
+
+        protected override Pos InizioOffset => new Pos(0,3); //posizione iniziale 
+    }
+}
